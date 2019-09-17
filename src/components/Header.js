1921094cc85b025bfa980e92bbcb846f2583109e
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-09-17 16:03:21 +0100

    Header styling and structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,28 @@
 import React from "react";
 
 const Header = props => (
-  <header>
-    <h1>Pukka Wax Radio</h1>
+  <header className="black mb5 pt5">
+    <h1 className="ttu f3 tracked-mega-ns anton tc mt0 mb3">Pukka Wax Radio</h1>
 
     <nav>
-      <ul>
-        <li>Selekta</li>
-        <li>Vaults</li>
-        <li>Scripture</li>
+      <ul className="list flex justify-center">
+        <li className="mh2">
+          <a className="nav-link link biriyani-black f6 ttu gray active">
+            Selekta
+          </a>
+        </li>
+        <li className="mh2">
+          <a className="nav-link link biriyani-black f6 ttu gray">
+            Vaults
+          </a>
+        </li>
+        <li className="mh2">
+          <a className="nav-link link biriyani-black f6 ttu gray">
+            Scripture
+          </a>
+        </li>
       </ul>
     </nav>
-
   </header>
 );
 
