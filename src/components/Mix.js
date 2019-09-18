7548ebd59fb62074a45e589864bf97ef56978bb4
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-09-18 16:22:53 +0100

    PlayMix component wrap around the Mix component to handle the mixPlay function on click

--- src/components/Mix.js
+++ src/components/Mix.js
@@ -1,19 +1,19 @@
 import React from "react";
 import PlayButton from "./PlayButton";
+import PlayMix from "./PlayMix";
 
-const Mix = ({ name, playMix, currentMix, id }) => (
+const Mix = ({ name, ...props }) => (
   <div
     className="aspect-ratio aspect-ratio--3x4 pointer"
-    onClick={() => playMix(id, true)}
   >
-    <div className="ph3 pv4 aspect-ratio--object mix-overlay">
-      <div className="flex items-center relative z-2">
-        <h1 className="f4 f3-l mv0 white ttu biryani pr2 lh-title">
-          {name}
-        </h1>
-        <PlayButton />
+    <PlayMix {...props}>
+      <div className="ph3 pv4 aspect-ratio--object mix-overlay">
+        <div className="flex items-center relative z-2">
+          <h1 className="f4 f3-l mv0 white ttu biryani pr2 lh-title">{name}</h1>
+          <PlayButton />
+        </div>
       </div>
-    </div>
+    </PlayMix>
   </div>
 );
 
