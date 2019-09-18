e8172e26f078266ca9947a24dac57f86a399efd9
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-09-18 17:39:52 +0100

    Use API data to set mix background image

--- src/components/Mix.js
+++ src/components/Mix.js
@@ -2,12 +2,15 @@ import React from "react";
 import PlayButton from "./PlayButton";
 import PlayMix from "./PlayMix";
 
-const Mix = ({ name, ...props }) => (
-  <div className="aspect-ratio aspect-ratio--3x4 pointer">
+const Mix = ({ name, mix, ...props }) => (
+  <div
+    className="aspect-ratio aspect-ratio--3x4 pointer cover bg-center"
+    style={{ backgroundImage: `url(${mix.pictures.extra_large})` }}
+  >
     <PlayMix {...props}>
       <div className="ph3 pv4 aspect-ratio--object mix-overlay">
         <div className="flex items-center relative z-2">
-          <h1 className="f4 f3-l mv0 white ttu biryani pr2 lh-title">{name}</h1>
+          <h1 className="f4 f3-l b mv0 white ttu biryani pr2 lh-title">{name}</h1>
           <PlayButton {...props} />
         </div>
       </div>
