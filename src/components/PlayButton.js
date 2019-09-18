09d4ada45226875245d6b869ddfc525fd332c4e6
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-09-18 15:42:57 +0100

    Add play button component and CSS

---
+++ src/components/PlayButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PlayButton = props => (
+  <div>
+    <button className="button bg-transparent contain play-button db bg-contain bg-center pointer center"></button>
+  </div>
+);
+
+export default PlayButton;
