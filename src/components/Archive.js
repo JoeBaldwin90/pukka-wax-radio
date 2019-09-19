fbde6d9bb89e6b68330ffe44a22e42160ec97f11
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-09-19 12:38:07 +0100

    Wrap each list element with the PlayMix component to play the selected mix based on it's ID

--- src/components/Archive.js
+++ src/components/Archive.js
@@ -1,15 +1,18 @@
 import React from "react";
 import PlayButton from "./PlayButton"
+import PlayMix from "./PlayMix";
 
 const Archive = ({ mixes, ...props }) => (
-  <ul className="list pl0 archive">
+  <ul className="list pl0 archive mb5">
     {mixes.map(mix => (
+      <PlayMix {...props} id={mix.key}>
         <li className="ph3 ph4-l">
           <div className="pv3 bb b--light-gray flex justify-between items-center">
             <h1 className="f6 mv0 black b ttu biriyani pr3">{mix.name}</h1>
             <PlayButton />
           </div>
         </li>
+      </PlayMix>
     ))}
   </ul>
 );
