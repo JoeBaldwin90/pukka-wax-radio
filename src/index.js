29ba285d5989710d4888aff2344d27e8bc0ccffc
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-09-20 15:50:56 +0100

    Set up store and initial state

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import "tachyons";
-import './css/main.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import "./css/main.css";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
+import mixesApp from "./store";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  mixesApp,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const Root = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(<Root />, document.getElementById("root"));
 
 serviceWorker.unregister();
