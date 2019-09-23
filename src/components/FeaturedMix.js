ef135585fb46289fdd5be608da49797103bd7b41
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-09-23 18:30:58 +0100

    Add default background colors to background images

--- src/components/FeaturedMix.js
+++ src/components/FeaturedMix.js
@@ -4,10 +4,21 @@ import { Link } from "react-router-dom";
 import PlayButton from "./PlayButton";
 import PlayMix from "./PlayMix";
 
-const FeaturedMix = ({ name, pictures = {}, title, id, slug, ...props }) => (
+const FeaturedMix = ({
+  name,
+  pictures = {},
+  picture_primary_color,
+  title,
+  id,
+  slug,
+  ...props
+}) => (
   <div
     className="w-50-l vh-100 flex items-center justify-center cover bg-center pad-bottom fixed-l left-0 mix-overlay"
-    style={{ backgroundImage: `url(${pictures.extra_large})` }}
+    style={{
+      backgroundImage: `url(${pictures.extra_large})`,
+      backgroundColor: `#${picture_primary_color}`
+    }}
   >
     <div className="w-100 tc pa3 relative z-2">
       <p className="b biriyani f6 white ttu">{title}</p>
