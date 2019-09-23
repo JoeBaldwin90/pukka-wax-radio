ee0cea728cbb120c8d0c24b3eda934252fcb36e8
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-09-23 18:19:37 +0100

    Dynamic featured mix title

--- src/components/FeaturedMix.js
+++ src/components/FeaturedMix.js
@@ -3,14 +3,14 @@ import { connect } from "react-redux";
 import PlayButton from "./PlayButton";
 import PlayMix from "./PlayMix";
 
-const FeaturedMix = ({ name, pictures = {}, ...props }) => (
+const FeaturedMix = ({ name, pictures = {}, title, ...props }) => (
   <div
     className="w-50-l vh-100 flex items-center justify-center cover bg-center pad-bottom fixed-l left-0 mix-overlay"
     style={{ backgroundImage: `url(${pictures.extra_large})` }}
   >
     <PlayMix {...props}>
       <div className="w-100 tc pa3 relative z-2">
-        <p className="b biriyani f6 white ttu">Featured Mix</p>
+        <p className="b biriyani f6 white ttu">{title}</p>
         <h1 className="f1-ns f2 lh-title mv2 mh5-l anton white ttu">{name}</h1>
         <PlayButton />
       </div>
@@ -32,6 +32,17 @@ const getMix = state => {
   return featuredMix || firstMix; // Returen featuredMix if exists, else return firstMix
 };
 
+const getTitle = state => {
+  if (state.featuredMix) {
+    return "Currently viewing";
+  } else if (state.currentMix && state.playing) {
+    return "Currently playing";
+  } else {
+    return "Featured mix";
+  }
+};
+
 export default connect(state => ({
-  ...getMix(state)
+  ...getMix(state),
+  title: getTitle(state)
 }))(FeaturedMix);
