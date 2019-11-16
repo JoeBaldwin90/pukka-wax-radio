91f58535eeeb88375ee8eb2c1bce35d3b4888edb
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-11-16 21:40:47 +0000

    Convert site header and links to styled components.
    Because I had to work with a third-party component – NavLink from the
    React router DOM – I had to create a new component "Link" so it could
    be styled.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,44 +1,74 @@
 import React from "react";
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
-const Header = props => (
-  <header className="black mb5 pt5">
-    <NavLink exact to="/" className="link black">
-      <h1 className="ttu f3 tracked-mega-ns anton tc mt0 mb3">
-        Pukka Wax Radio
-      </h1>
-    </NavLink>
+const Header = styled.header`
+  margin-bottom: 4em;
+  padding-top: 4em;
+`;
 
-    <nav>
-      <ul className="list flex justify-center">
-        <li className="mh2">
-          <NavLink
-            exact
-            to="/"
-            className="nav-link link biriyani-black f6 ttu gray"
-          >
-            What's Hot
-          </NavLink>
-        </li>
-        <li className="mh2">
-          <NavLink
-            to="/archive"
-            className="nav-link link biriyani-black f6 ttu gray"
-          >
-            Archive
-          </NavLink>
-        </li>
-        <li className="mh2">
-          <NavLink
-            to="/about"
-            className="nav-link link biriyani-black f6 ttu gray"
-          >
-            About
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  </header>
+const PukkaTitle = styled.h1`
+  font-family: "Anton", sans-serif;
+  font-size: 1.5rem;
+  text-transform: uppercase;
+  text-align: center;
+  margin-bottom: 1rem;
+  text-decoration: none;
+
+  @media (min-width: 30em) {
+    letter-spacing: 0.25em;
+  }
+`;
+
+const LinksContainer = styled.ul`
+  display: flex;
+  justify-content: center;
+  list-style-type: none;
+`;
+
+const ListEl = ({ key, children }) => (
+  <li key={key} className="mh2">
+    {children}
+  </li>
 );
 
-export default Header;
+const Link = ({ to, className, children }) => (
+  <NavLink exact to={to} className={className}>
+    {children}
+  </NavLink>
+);
+
+const StyledLink = styled(Link)`
+  font-size: 0.875rem;
+  text-transform: uppercase;
+  text-decoration: none;
+  border-bottom: solid 3px rgba(0, 0, 0, 0);
+  color: #777;
+  transition: color 0.15s ease-in;
+
+  :hover {
+    color: #000;
+  }
+`;
+
+export default () => (
+  <Header>
+    <Link to="/" className="link black">
+      <PukkaTitle>Pukka Wax Radio</PukkaTitle>
+    </Link>
+
+    <nav>
+      <LinksContainer>
+        <ListEl key="1">
+          <StyledLink to="/">What's Hot</StyledLink>
+        </ListEl>
+        <ListEl key="2">
+          <StyledLink to="/archive">Archive</StyledLink>
+        </ListEl>
+        <ListEl key="3">
+          <StyledLink to="/about">About</StyledLink>
+        </ListEl>
+      </LinksContainer>
+    </nav>
+  </Header>
+);
