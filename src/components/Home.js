5c7f9247e3d47258011f93ff2fdd60c44fd90c19
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-11-16 20:37:06 +0000

    Home / What's hot converted to styled components.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,25 @@
 import React from "react";
 import { connect } from "react-redux";
+import styled from "styled-components";
 import Mix from "./Mix";
 
+const WhatsHot = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 0 1em 5em 1em;
+
+  @media (min-width: 60em) {
+    padding: 0 2em 5em 2em;
+  }
+`;
+
 const Home = ({ mixes, ...props }) => (
-  <div className="flex flex-wrap justify-between ph3 ph4-l mixes">
-    {/* Map over all of the mixes */}
-    {mixes.slice(0,4).map(mix => (
-      <div className="mix mb5">
-        <Mix {...props} {...mix} />
-      </div>
+  <WhatsHot>
+    {mixes.slice(0, 4).map(mix => (
+      <Mix {...props} {...mix} />
     ))}
-  </div>
+  </WhatsHot>
 );
 
 export default connect(state => state)(Home);
--- src/components/Mix.js
+++ src/components/Mix.js
@@ -1,25 +1,77 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 import PlayButton from "./PlayButton";
 import PlayMix from "./PlayMix";
 
-const Mix = ({ name, pictures, slug, id, ...props }) => (
-  <div
-    className="aspect-ratio aspect-ratio--3x4 pointer cover bg-center"
-    style={{ backgroundImage: `url(${pictures.extra_large})` }}
-  >
-    <div className="ph3 pv4 aspect-ratio--object mix-overlay">
-      <div className="flex flex-column relative z-2">
-        <h1 className="f5 f3-ns b mv0 white ttu biryani pr2 lh-title">
-          {name}
-        </h1>
-      </div>
-      <Link to={`/show/${slug}`} className="absolute absolute--fill z-3" />
-      <PlayMix id={id} className="absolute bottom-1 left-1 z-5 flex items-left pointer">
-        <PlayButton />
-      </PlayMix>
-    </div>
-  </div>
-);
+const HomepageMixContainer = styled.div`
+  width: calc(100% - 2em);
+  margin: 0 auto 2em auto;
 
-export default Mix;
+  @media (min-width: 30em) {
+    width: calc(50% - 2em);
+    margin-bottom: 4em;
+
+    &:nth-of-type(even) {
+      transform: translateY(4rem);
+    }
+  }
+`;
+
+const MixBackground = styled.div`
+  height: 0;
+  position: relative;
+  padding-bottom: 133.33%;
+  background-size: cover !important;
+  background-repeat: no-repeat;
+  background-position: center center;
+`;
+
+const MixOverlay = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  padding: 2em 1em;
+  z-index: 100;
+
+  :after {
+    content: "";
+    position: absolute;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0));
+  }
+`;
+
+const MixTitle = styled.h2`
+  font-family: "Biryani", sans-serif;
+  font-size: 1.5em;
+  line-height: 1.2;
+  font-weight: 900;
+  color: #fff;
+  text-transform: uppercase;
+  padding-right: 0.5em;
+
+  position: relative;
+  z-index: 2;
+`;
+
+export default ({ name, pictures, slug, id }) => (
+  <HomepageMixContainer>
+    <MixBackground style={{ backgroundImage: `url(${pictures.extra_large})` }}>
+      <MixOverlay>
+        <MixTitle>{name}</MixTitle>
+        <Link to={`/show/${slug}`} className="absolute absolute--fill z-3" />
+        <PlayMix
+          id={id}
+          className="absolute bottom-1 left-1 z-5 flex items-left"
+        >
+          <PlayButton />
+        </PlayMix>
+      </MixOverlay>
+    </MixBackground>
+  </HomepageMixContainer>
+);
